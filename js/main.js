96d9773338220ed7f82817e814fa617594245cd8
Alexander <lyotchik1@mail.ru>
2016-11-03 16:45:35 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -8,19 +8,38 @@ function changeBar(x) {
     }
 }
 
-function showModal(x) {
-	
-	// Get the modal
-	var modal = document.getElementById('myModal');
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
 
-	// Get the image and insert it inside the modal - use its "alt" text as a caption
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+ // Get the modal
+  var modal = document.getElementById('myModal');
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
 	//var img = document.getElementById('myImg');
-	var modalImg = document.getElementById("img01");
-	var captionText = document.getElementById("caption");
-	
-    modal.style.display = "block";
-    modalImg.src = x.src;
-    captionText.innerHTML = x.alt;
-	
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+
+  var slides = document.getElementsByClassName("mySlides");
+ 
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  var current = slides[slideIndex-1];   
+ 
+  modalImg.src = current.src;
+  captionText.innerHTML = current.alt;
 }
 
