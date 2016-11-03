3c54d8ce7efdab6ad701a0b10f09580e74021084
Alexander <lyotchik1@mail.ru>
2016-11-03 16:21:58 +0300

    modal image added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-function myFunction() {
+function changeBar(x) {
+	x.classList.toggle("change");
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
@@ -6,3 +7,20 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+function showModal(x) {
+	
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	//var img = document.getElementById('myImg');
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	
+    modal.style.display = "block";
+    modalImg.src = x.src;
+    captionText.innerHTML = x.alt;
+	
+}
+
